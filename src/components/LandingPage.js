31b907cd619948b78d16d9295297d73c07f12a38
GaneshDabade <gnxd555@gmail.com>
2022-09-04 20:19:45 +0530

    Change in CSS and LandingPage

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
 
 import '../App.css'
 import BackgroundImage from '../images/bg.jpg'
@@ -7,15 +9,38 @@ import BackgroundImage from '../images/bg.jpg'
 export default function LandingPage() {
     return (
         <header style={ HeaderStyle }>
-            <h1 className="main-title text-center">login / register page</h1>
-            <p className="main-para text-center">join us now and don't waste time</p>
-            <div className="buttons text-center">
-                <Link to="/login">
-                    <button className="primary-button">log in</button>
-                </Link>
-                <Link to="/register">
+            <h1 className="main-title text-center text-info">Voting-Chain</h1>
+            <p className="main-para text-center word">A Blockchain based E-Voting System,built with trust</p>
+            <div className='d-flex align-content-end flex-wrap' style={{marginLeft:'17rem',paddingTop:'1rem'}} >
+            <div style={{paddingRight:'5rem'}}>
+             <Card style={{ width: '18rem' }}>
+             <Card.Body className="text-center">
+             <Card.Title >Hii</Card.Title>
+             <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Link to="/login">
+                    <button className="primary-button" id="reg_btn"><span>log in</span></button>
+        </Link>
+      </Card.Body>
+    </Card>
+    </div>
+    <div style={{paddingLeft:'5rem'}}>
+    <Card style={{ width: '18rem' }}>
+             <Card.Body className="text-center">
+             <Card.Title>Card Title</Card.Title>
+             <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Link to="/register">
                     <button className="primary-button" id="reg_btn"><span>register </span></button>
-                </Link>
+            </Link>
+      </Card.Body>
+    </Card>
+         </div>       
+                
             </div>
         </header>
     )
